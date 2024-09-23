4e4404a8a3e77f8d11f278e3bfbb6dffdea2cf6e
Nola Gerold <redpendrew@gmail.com>
2024-09-22 20:44:34 -0700

    Start populating server.py

--- server.py
+++ server.py
@@ -3,3 +3,29 @@ import socket
 import socketserver
 import sys
 
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = str(self.request.recv(1024), 'ascii')
+        cur_thread = threading.current_thread()
+        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        self.request.sendall(response)
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+def main(arg = sys.argv):
+    HOST, PORT = "localhost", 0
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    with server:
+        ip, port = server.server_address 
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+        print("Server loop running in thread:", server_thread.name)
+
+        server.shutdown()
+  
+
+if __name__ == "__main__":
+    main()
