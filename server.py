05bf8add52d095d0ef4aca9953a9685b8f7c1838
Nola Gerold <redpendrew@gmail.com>
2024-09-28 16:02:13 -0700

    Start implementing the server's request handling system

--- server.py
+++ server.py
@@ -1,25 +1,39 @@
+import json
 import threading
 import socket
 import socketserver
 import sqlite3
 import sys
 
+dbcon = sqlite3.connect("server.db")
+dbcur = dbcon.cursor()
+
+
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
+        data = json.loads(str(self.request.recv(1024), 'utf-8'))
+        response = ""
+        match data["type"]:
+            case "REG" | "REGISTER":
+                dbcur.execute()
+                response = {
+                    "type": ""
+                }
+            case "MSG" | "MESSAGE":
+                response = "PLACEHOLDER"
+            case "GET" | "FETCH":
+                response = "PLACEHOLDER"
         cur_thread = threading.current_thread()
-        print(f"Replying to {} on {cur_thread}", 0)
-        response = bytes(
-                "{}: {}".format(cur_thread.name, data),
-                'ascii'
-                )
-        self.request.sendall(response)
+        print(f"Replying on {cur_thread}")
+        self.request.sendall(bytes(response, 'utf-8'))
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 def main(arg = sys.argv):
+    dbcur.execute("CREATE TABLE messages ( Timestamp int, UserName varchar(255), Content varchar(255) )")
+    dbcur.execute("CREATE TABLE users ( UserID int, UserName varchar(255) )")
     HOST, PORT = "localhost", 0
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     with server:
