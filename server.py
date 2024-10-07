cf608927b91c04f6a35f44262657de61f5a2ffd7
Nola G <gingerwitch64@gmail.com>
2024-10-06 20:07:25 -0700

    Fix several problems/inconsistencies with db operations + add prompt

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 import threading
 import socket
 import socketserver
@@ -6,18 +7,19 @@ import sqlite3
 import time
 
 # Autocommit is enabled due to the proof-of-concept nature of this project
-dbcon = sqlite3.connect("server.db", autocommit=True)
-dbcur = dbcon.cursor()
-
+SQLITE_FILEPATH = "server.db"
+SQLITE_AUTOCOMMIT = True
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
+        dbcon = sqlite3.connect(SQLITE_FILEPATH, autocommit=SQLITE_AUTOCOMMIT)
+        dbcur = dbcon.cursor()
         data = json.loads(str(self.request.recv(1024), 'utf-8'))
         response = {}
         match data["type"]:
             case "REG" | "REGISTER":
-                userTaken = not ((dbcur.execute("SELECT UserName FROM users WHERE UserName='?'", (data["username"],)).fetchone() is None)
-                             and (dbcur.execute("SELECT UserName FROM users WHERE UserID='?'",   (data["userid"],  )).fetchone() is None))
+                userTaken = not ((dbcur.execute("SELECT UserName FROM users WHERE UserName=?", (data["username"],)).fetchone() is None)
+                             and (dbcur.execute("SELECT UserName FROM users WHERE UserID=?",   (data["userid"],  )).fetchone() is None))
                 if not userTaken:
                     dbcur.execute("INSERT INTO users VALUES (?, ?)", (data["userid"], data["username"],))
                 response = {
@@ -25,7 +27,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     "status": (userTaken * 1), # convert the result bool into an integer status code
                 }
             case "MSG" | "MESSAGE":
-                username = dbcur.execute("SELECT UserName FROM users WHERE UserID='?'", (data["userid"],)).fetchone()
+                username = dbcur.execute("SELECT UserName FROM users WHERE UserID=?", (data["userid"],)).fetchone()
                 useridDNE = username is None
                 if not useridDNE:
                     username = username[0]
@@ -41,31 +43,41 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                     "status": 0,
                 }
                 if "since" in data:
-                    response["messages"] = dbcur.execute("SELECT * FROM messages WHERE Timestamp >= ?", data["since"]).fetchall()
+                    response["messages"] = dbcur.execute("SELECT * FROM messages WHERE Timestamp >= ?", (data["since"],)).fetchall()
                 elif "last" in data:
-                    response["messages"] = dbcur.execute("SELECT * FROM messages ORDER BY Timestamp DESC LIMIT ?", int(data["last"])).fetchall()
+                    response["messages"] = dbcur.execute("SELECT * FROM messages ORDER BY Timestamp DESC LIMIT ?", (int(data["last"]),)).fetchall()
                 else:
                     response["messages"] = dbcur.execute("SELECT * FROM messages ORDER BY Timestamp DESC LIMIT 1").fetchone()
 
         cur_thread = threading.current_thread()
         print(f"Replying on {cur_thread}")
         self.request.sendall(bytes(json.dumps(response), 'utf-8'))
+        dbcon.close()
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 def main(host="localhost",port=5500):
-    dbcur.execute("CREATE TABLE messages ( Timestamp int, UserName varchar(255), Content varchar(255) )")
-    dbcur.execute("CREATE TABLE users ( UserID char(32), UserName varchar(255) )")
+    if not os.path.isfile(SQLITE_FILEPATH):
+        print("")
+        dbcon = sqlite3.connect(SQLITE_FILEPATH, autocommit=SQLITE_AUTOCOMMIT)
+        dbcur = dbcon.cursor()
+        dbcur.execute("CREATE TABLE messages ( Timestamp int, UserName varchar(255), Content varchar(255) )")
+        dbcur.execute("CREATE TABLE users ( UserID char(32), UserName varchar(255) )")
+        dbcon.close()
     server = ThreadedTCPServer((host, port), ThreadedTCPRequestHandler)
     with server: 
         server_thread = threading.Thread(target=server.serve_forever)
         server_thread.daemon = True
         server_thread.start()
         print("Server loop running in thread:", server_thread.name)
+        shutoff = False
+        while not shutoff:
+            prompt = input("Prompt is running")
+            if prompt in ["quit", "exit", "stop", "shutdown"]:
+                shutoff = True
 
         server.shutdown()
-  
 
 if __name__ == "__main__":
     main()
